4431f4dd80a8a179dce1ac02efac2423dc84adee
ozsoymu001 <ozsoymu001@ptg.local>
2024-11-23 12:09:01 +0300

    files added.

---
+++ main.py
@@ -0,0 +1,25 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class PongGame(Widget):
+    pass
+
+class PongBall(Widget):
+
+    # velocity of the ball on x and y axis
+    vel_x = NumericProperty(0)
+    vel_y = NumericProperty(0)
+
+    # referencelist property so we can use ball.velocity as a shorthand, 
+    # just like e.g. w.pos for w.x and w.y
+
+    
+
+
+
+class PongApp(App):
+    def build(self):
+        return PongGame()
+    
+if __name__ == '__main__':
+    PongApp().run()
