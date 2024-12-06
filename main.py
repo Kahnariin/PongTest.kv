9d23ea5445208633eb9e1e7331835ee86428cd21
kahnariin <m.ikbalozsoy@gmail.com>
2024-12-06 17:35:43 +0300

    files created

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from kivy.uix.widget import Widget
 class PongGame(Widget):
     pass
 
-class PongBall(Widget):
+#class PongBall(Widget):
 
     # velocity of the ball on x and y axis
-    vel_x = NumericProperty(0)
-    vel_y = NumericProperty(0)
+    #vel_x = NumericProperty(0)
+    #vel_y = NumericProperty(0)
 
     # referencelist property so we can use ball.velocity as a shorthand, 
     # just like e.g. w.pos for w.x and w.y
